321faac0c64f5255a95463a0a162c83c9c4e639c
Dmytro Bezuhlyi | CHISW JS Dev <dmytro.bezuhlyi@chisw,com>
2021-11-26 10:08:21 +0200

    add props and view more button

--- src/components/BlogArticle.vue
+++ src/components/BlogArticle.vue
@@ -3,27 +3,27 @@
     <el-col :xs="23" :sm="22" :md="20" :lg="18" :xl="18" class="post-view">
       <el-row type="flex" justify="center">
         <el-col :span="12">
-          <h3>Article Title</h3>
+          <h3>{{ item.title }}</h3>
         </el-col>
       </el-row>
 
       <el-row type="flex" justify="center" class="post-sub-title">
         <el-col :span="5">
           Destination:<br />
-          <strong>&lt;destination&gt;</strong>
+          <strong>{{ item.destination }}</strong>
         </el-col>
         <el-col :span="5">
           Author:<br />
-          <strong>&lt;author&gt;</strong>
+          <strong>{{ item.author }}</strong>
         </el-col>
         <el-col :span="5">
           Posted date:<br />
-          <strong>Posted time</strong>
+          <strong>{{ item.postTime }}</strong>
         </el-col>
       </el-row>
       <el-row type="flex" justify="center">
         <el-col :span="16">
-          <img src="" alt="article image" />
+          <img :src="item.image" alt="article image" />
         </el-col>
       </el-row>
       <el-row type="flex" justify="center">
@@ -33,16 +33,26 @@
       </el-row>
       <el-row type="flex" justify="center">
         <el-col class="post-button" :span="12">
-          <el-button>Read More </el-button>
+          <el-button @click="viewMore(`${item.id}`, item)">Read More</el-button>
         </el-col>
       </el-row>
     </el-col>
   </el-row>
 </template>
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { Item } from "@/types/index.ts";
 
 @Component
-export default class BlogArticle extends Vue {}
+export default class BlogArticle extends Vue {
+  @Prop() item: Item | undefined;
+
+  viewMore(id: string, item: Item): void {
+    this.$router.push({
+      name: "ArticlePage",
+      params: { id, item },
+    });
+  }
+}
 </script>
 <style lang="scss"></style>
